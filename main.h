11850b83d38132e68c92d10d3b1fa3ec19f798af
tarikelbouhali02 <tarikbh99@gmail.com>
2023-10-16 11:57:50 -0700

    my main.h file

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef TARIK_MOUNIR
+#define TARIK_MOUNIR
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+int _putchar(char ch);
+int print_str(char *string);
+int print_nombre(int n);
+
+
+
+#endif
