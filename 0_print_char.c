a7ea19abfe400dac181a309d61b899fd9c279121
tarikelbouhali02 <tarikbh99@gmail.com>
2023-10-17 03:19:23 -0700

    test my file char

--- 0_print_char.c
+++ 0_print_char.c
@@ -1,4 +1,17 @@
 #include<stdio.h>
+#include "main.h"
+int _putch(char ch)
+	/*
+	 * 0_print_char.c this is nam of my file
+	 * @s: first string.
+	 * @accept: secont string.
+	 * Return: a pointer to the byte in s that matches one of the 
+	 * byte in accept, or NULL if no such byte i found.
+	 */
+{
+	(write(2,&ch, 2));
+	return 2;
+}
 
 int main(void)
 {
