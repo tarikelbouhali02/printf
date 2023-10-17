9842d29a2a7c92bb04202d1b49ff509955795108
tarikelbouhali02 <tarikbh99@gmail.com>
2023-10-17 10:58:52 -0700

    This is my file printf.c

--- _printf.c
+++ _printf.c
@@ -1,53 +1,40 @@
-#include<stdio.h>
+#include "main.h"
+#include "holberton.h"
+
+/**
+ * _printf - Custom printf function that processes a format string
+ * and its arguments to print formatted output.
+ * @format: A format string containing format specifiers.
+ * Return: The total count of characters printed.
+ */
+
 
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list args;
-	va_start(args, format);
-	if (!format || !format[0])
+	int printed_chars;
+	conver_t f_list[] = {
+		{"c", print_char},
+		{"s", print_string},
+		{"%", print_percent},
+		{"t", print_integer},
+		{"u", print_integer},
+		{"k", print_binary},
+		{"y", print_reversed},
+		{"h", rot13},
+		{"m", unsigned_integer},
+		{"k", print_octal},
+		{"m", print_hex},
+		{"y", print_heX},
+		{NULL, NULL}
+	};
+	va_list arg_list;
+
+	if (format == NULL)
 		return (-1);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if(*format == 'c')// print some character
-			{
-				char cha = va_args(args, int);
-				_putch(cha);
-				count++;
-				format++;
-			}
-			else if (*format == 's') //print spme argument string
-			{
-				char *string = va_arg(args, char*);
-				int a = printf_str(string);
-				count += a;
-				format++;
-			}
-			else if (*format == '%') //for %% print
-			{
-				_putch('%');
-				count++;
-				format++;
-			}
-			else //%string or other character
-			{
-				if (*format)
-				{
-					format--;
-					_putch(*format);
-					format++;
-				}
-				count++;
-			}
-		}
-		else {
-			_putch(*format);
-			count++;
-			format++;
-		}
-	}
-	return count;
+
+	va_start(arg_list, format);
+	/*Calling parser function*/
+	printed_chars = parser(format, f_list, arg_list);
+	va_end(arg_list);
+	return (printed_chars);
 }
