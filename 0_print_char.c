8e5fc941c6ef093536677300da1981d7f25cdd88
tarikelbouhali02 <tarikbh99@gmail.com>
2023-10-17 03:04:35 -0700

    i try this code

---
+++ 0_print_char.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include"main.h"
+
+
+int _putch(char ch)
+/*
+ * 0-print_char.c this is some name of the file
+ * @s: first string
+ * @accept: second string
+ * @accept:thrd string.
+ * return: a pointer to the byte in s that matches one of the
+ * byte in accept, or NULL if no suchbyte i found.
+ */
+
+{
+	printf("hey is this character!");
+	return 0;
+}
