3fba9cbf268fe10eb50f8c352c3f0bacead87933
tarikelbouhali02 <tarikbh99@gmail.com>
2023-10-18 14:17:37 -0700

    1

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef DRISS_TARIK_H
+#define DRISS_TARIK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+int _printf(const char *format, ...);
+int _putch(char ch);
+int print_str(char *string);
+int print_nombre(int n);
+int long_nbr(int nbr);
+
+#endif
