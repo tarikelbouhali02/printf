25a36c07bdf30d41933354865046dcc05a1cea44
tarikelbouhali02 <tarikbh99@gmail.com>
2023-10-18 14:15:04 -0700

    1

---
+++ _printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string>
+#include<vector>
+using namespace std;
+struct student;
+string name;
+float note;
+};
+int main() {
+    stuent s1;
+    s1.name = "tarik"
+    s1.note = 19;
+    cout << s1.name;
+
+
+}
