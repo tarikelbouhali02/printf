526b12eacb8bfba2bb9e2c00a5e5afd283f69336
tarikelbouhali02 <tarikbh99@gmail.com>
2023-10-18 14:16:34 -0700

    1

--- _printf.c
+++ _printf.c
@@ -1,16 +1,66 @@
-#include<stdio.h>
-#include<string>
-#include<vector>
-using namespace std;
-struct student;
-string name;
-float note;
-};
-int main() {
-    stuent s1;
-    s1.name = "tarik"
-    s1.note = 19;
-    cout << s1.name;
+#include "main.h"
 
+int _printf(const char *format, ...)
+{
+    int co = 0;
 
+    va_list args;
+    va_start(args, format);
+    if (!format || !format[0])
+        return (-1);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (*format == 'c') // print character
+            {
+                char cha = va_arg(args, int);
+                _putch(cha);
+                co++;
+                format++;
+            }
+            else if (*format == 's') // print argument string
+            {
+                char *string =  va_arg(args, char*);
+                int a = print_str(string);
+                co += a;
+                format++;
+            }
+            else if (*format == '%') //for %% print %
+            {
+                _putch('%');
+                co++;
+                format++;
+            }
+            else if (*format == 'd' || *format == 'i')
+            {
+                int nombre = va_arg(args, int);
+                int nco;
+                if (nombre < 0)
+                    co++;
+                nco = long_nbr(nombre);
+                format++;
+                co =co + nco;
+                print_nombre(nombre);
+            }
+            else // %string or other character
+            {
+                if (*format)
+                {
+                    format--;
+                    _putch(*format);
+                    format++;
+                }
+                co++;
+            }
+        }
+        else
+        {
+            _putch(*format);
+            co++;
+            format++;
+        }
+    }
+    return co;
 }
